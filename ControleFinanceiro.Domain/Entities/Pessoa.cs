ed095783ee23a0282378c77f782c5f8892280ae1
Matheus-araujo-dev <matheus.araujodev13@gmail.com>
2025-07-31 11:46:29 -0300

    Add core financial domain entities

---
+++ ControleFinanceiro.Domain/Entities/Pessoa.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleFinanceiro.Domain.Entities
+{
+    public class Pessoa : BaseEntity
+    {
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(14)]
+        public string Documento { get; set; }
+
+        public DateTime? DataNascimento { get; set; }
+
+        public ICollection<Cartao> Cartoes { get; set; } = new List<Cartao>();
+        public ICollection<ContaPagar> ContasPagar { get; set; } = new List<ContaPagar>();
+        public ICollection<ContaReceber> ContasReceber { get; set; } = new List<ContaReceber>();
+        public ICollection<MovimentacaoFinanceira> MovimentacoesFinanceiras { get; set; } = new List<MovimentacaoFinanceira>();
+    }
+}
