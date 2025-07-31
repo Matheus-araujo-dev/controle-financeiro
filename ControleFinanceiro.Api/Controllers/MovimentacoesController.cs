ad076abe3bcc43b54a109ec0fb335ceabf0a56c1
Matheus-araujo-dev <matheus.araujodev13@gmail.com>
2025-07-31 12:10:44 -0300

    Add REST controllers and configure services

---
+++ ControleFinanceiro.Api/Controllers/MovimentacoesController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ControleFinanceiro.Application.Services;
+using ControleFinanceiro.Domain.Entities;
+
+namespace ControleFinanceiro.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovimentacoesController : ControllerBase
+    {
+        private readonly IMovimentacaoFinanceiraAppService _service;
+
+        public MovimentacoesController(IMovimentacaoFinanceiraAppService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<MovimentacaoFinanceira> GetById(Guid id)
+        {
+            var mov = _service.GetById(id);
+            if (mov == null)
+                return NotFound();
+            return Ok(mov);
+        }
+
+        [HttpGet("pessoa/{pessoaId}")]
+        public ActionResult<IEnumerable<MovimentacaoFinanceira>> GetByPessoa(Guid pessoaId)
+        {
+            return Ok(_service.GetByPessoa(pessoaId));
+        }
+
+        [HttpGet("periodo")]
+        public ActionResult<IEnumerable<MovimentacaoFinanceira>> GetByPeriodo([FromQuery] DateTime inicio, [FromQuery] DateTime fim)
+        {
+            return Ok(_service.GetByPeriodo(inicio, fim));
+        }
+
+        [HttpGet("tipo/{tipo}")]
+        public ActionResult<IEnumerable<MovimentacaoFinanceira>> GetByTipo(TipoMovimentacao tipo)
+        {
+            return Ok(_service.GetByTipo(tipo));
+        }
+
+        [HttpPost]
+        public IActionResult Create(MovimentacaoFinanceira mov)
+        {
+            _service.Add(mov);
+            return CreatedAtAction(nameof(GetById), new { id = mov.Id }, mov);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, MovimentacaoFinanceira mov)
+        {
+            if (id != mov.Id)
+                return BadRequest();
+            _service.Update(mov);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            _service.Delete(id);
+            return NoContent();
+        }
+    }
+}
--- ControleFinanceiro.Api/Program.cs
+++ ControleFinanceiro.Api/Program.cs
@@ -1,4 +1,7 @@
 using ControleFinanceiro.Infrastructure.Data;
+using ControleFinanceiro.Application.Services;
+using ControleFinanceiro.Domain.Repositories;
+using ControleFinanceiro.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace ControleFinanceiro.Api
@@ -12,7 +15,33 @@ namespace ControleFinanceiro.Api
             builder.Services.AddDbContext<FinanceiroDbContext>(options =>
                 options.UseInMemoryDatabase("FinanceiroDb"));
 
+            builder.Services.AddScoped<IPessoaRepository, PessoaRepository>();
+            builder.Services.AddScoped<ICartaoRepository, CartaoRepository>();
+            builder.Services.AddScoped<IContaPagarRepository, ContaPagarRepository>();
+            builder.Services.AddScoped<IContaReceberRepository, ContaReceberRepository>();
+            builder.Services.AddScoped<IMovimentacaoFinanceiraRepository, MovimentacaoFinanceiraRepository>();
+            builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+
+            builder.Services.AddScoped<IPessoaAppService, PessoaAppService>();
+            builder.Services.AddScoped<ICartaoAppService, CartaoAppService>();
+            builder.Services.AddScoped<IContaPagarAppService, ContaPagarAppService>();
+            builder.Services.AddScoped<IContaReceberAppService, ContaReceberAppService>();
+            builder.Services.AddScoped<IMovimentacaoFinanceiraAppService, MovimentacaoFinanceiraAppService>();
+            builder.Services.AddScoped<ITransactionAppService, TransactionAppService>();
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.MapControllers();
 
             app.Run();
         }
